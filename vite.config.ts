c72f8c759baef88eaa466c628621d19a03316d42
Nick McCurdy <nick@nickmccurdy.com>
2022-06-26 03:59:59 -0400

    Improve production build settings

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,16 @@ import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/npm-versions/",
+  base: "./",
+  server: {
+    open: true,
+  },
+  build: {
+    target: "esnext",
+    sourcemap: true,
+  },
+  preview: {
+    open: true,
+  },
   plugins: [react()],
 })
