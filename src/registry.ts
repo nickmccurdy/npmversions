726d7c36982e670982de4a2ab4df785f71c2410d
Nick McCurdy <nick@nickmccurdy.com>
2022-08-03 07:11:37 -0400

    Improve inference

--- src/registry.ts
+++ src/registry.ts
@@ -1,11 +1,11 @@
 export type Downloads = Record<string, number>
 
-export interface Versions {
+interface Versions {
   package: string
   downloads: Downloads
 }
 
-export async function fetchDownloads(name: string): Promise<Downloads> {
+export async function fetchDownloads(name: string) {
   const response = await fetch(
     `https://api.npmjs.org/versions/${encodeURIComponent(name)}/last-week`,
   )
